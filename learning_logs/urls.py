38edb9a92bdece1ebcc531c1fdf95d81f25c66f3
daraffleur <romaniuk.dana@gmail.com>
2020-08-04 16:16:33 +0300

    Fix topic argument in url file

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
-    path('topics/<topic_id>/', views.topics, name='topic'),
+    path('topics/<topic_id>/', views.topic, name='topic'),
 
 ]
